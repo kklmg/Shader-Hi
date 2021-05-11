4ce0a332c4b00ed073eec0a465b1bc3bf7c3e783
kklmg <lm1522820366@gmail.com>
2021-05-12 02:30:08 +0900

    made a simple URP Shader

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+    
+    [SerializeField] 
+    private Transform[] points;
+
+    [SerializeField,Range(10, 100)]
+    int resolution = 10;
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) 
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI *  (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
