05a8cd94a9014a8bfb542a8edb082b55affdb389
kklmg <48175820+kklmg@users.noreply.github.com>
2021-07-13 17:31:14 +0900

    post processing

---
+++ Assets/Postprocessing/Postprocessing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Postprocessing : MonoBehaviour
+{
+    [SerializeField] private Material postprocessMaterial;
+
+    //method which is automatically called by unity after the camera is done rendering
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //draws the pixels from the source texture to the destination texture
+        Graphics.Blit(source, destination, postprocessMaterial);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
