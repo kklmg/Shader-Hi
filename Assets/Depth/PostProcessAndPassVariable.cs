4b7958fd5a41923f4f1373759358f4c35aff9ce8
kklmg <48175820+kklmg@users.noreply.github.com>
2021-09-10 19:03:54 +0900

    WaterFlooded

---
+++ Assets/Depth/PostProcessAndPassVariable.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PostProcessAndPassVariable  : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera _camera;
+
+    [SerializeField]
+    private Material postprocessMaterial;
+
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    void Start()
+    {
+        _camera.depthTextureMode = _camera.depthTextureMode | DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (postprocessMaterial)
+        {
+            PassFrustumDirection(postprocessMaterial);
+            //draws the pixels from the source texture to the destination texture
+            Graphics.Blit(src, dest, postprocessMaterial);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+    private void PassFrustumDirection(Material material)
+    {
+        if (material == null) return;
+
+        Camera cam = Camera.main;
+
+        //tan(fov/2)
+        float tanHalfFOV = Mathf.Tan(0.5f * cam.fieldOfView * Mathf.Deg2Rad);
+
+        float halfHeight = tanHalfFOV * cam.nearClipPlane;
+        float halfWidth = halfHeight * cam.aspect;
+
+        Vector3 toTop = cam.transform.up * halfHeight;
+        Vector3 toRight = cam.transform.right * halfWidth;
+        Vector3 forward = cam.transform.forward * cam.nearClipPlane;
+
+        Vector3 toTopLeft = forward + toTop - toRight;
+        Vector3 toBottomLeft = forward - toTop - toRight;
+        Vector3 toTopRight = forward + toTop + toRight;
+        Vector3 toBottomRight = forward - toTop + toRight;
+
+        // toTopLeft / nearPlane = cameraToDest / depth
+        // => cameraToDest = toTopLeft / nearPlane * depth  
+        toTopLeft /= cam.nearClipPlane;
+        toBottomLeft /= cam.nearClipPlane;
+        toTopRight /= cam.nearClipPlane;
+        toBottomRight /= cam.nearClipPlane;
+        
+
+        Matrix4x4 frustumDir = Matrix4x4.identity;
+        frustumDir.SetRow(0, toBottomLeft);
+        frustumDir.SetRow(1, toBottomRight);
+        frustumDir.SetRow(2, toTopLeft);
+        frustumDir.SetRow(3, toTopRight);
+        material.SetMatrix("_FrustumDir", frustumDir);
+    }
+}
---
+++ Assets/Depth/WaterFlooded.shader
@@ -0,0 +1,78 @@
+﻿Shader "Unlit/WaterFlooded"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _WaterColor("Water Color", Color) = (0,0,0.8,1)
+        _WaterHeight("Water Height", Float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float4 frustumDir : TEXCOORD1;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _WaterColor;
+            float _WaterHeight;
+            sampler2D _CameraDepthTexture;
+            float4x4 _FrustumDir;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                int ix = (int)o.uv.x;
+				int iy = (int)o.uv.y;
+
+                // uv = (0,0) => frustumDir = bottomLeft
+                // uv = (0,1) => frustumDir = bottomRight
+                // uv = (1,0) => frustumDir = topLeft
+                // uv = (1,1) => frustumDir = topRight
+				o.frustumDir = _FrustumDir[ix + 2 * iy];
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv.xy);
+                float depth = UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, i.uv));
+                float linearEyeDepth = LinearEyeDepth(depth);
+
+                float3 worldPos = _WorldSpaceCameraPos.xyz +  i.frustumDir * linearEyeDepth;
+                
+                if (worldPos.y < _WaterHeight)
+                return lerp(col, _WaterColor, _WaterColor.a); 
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
